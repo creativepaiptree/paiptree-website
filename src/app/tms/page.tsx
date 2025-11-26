e5860c695579c16e8948bc0157d4d848cb07b6f9
sundaynighttt <jazoo7777@gmail.com>
2025-11-26 16:14:53 +0900

    add pages6

--- src/app/tms/page.tsx
+++ src/app/tms/page.tsx
@@ -110,7 +110,7 @@ export default function TmsPage() {
         </div>
 
         {/* Scroll Indicator */}
-        <div className="absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce z-10">
+        <div className="absolute bottom-10 left-0 right-0 flex justify-center animate-bounce z-10">
           <svg className="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
           </svg>
