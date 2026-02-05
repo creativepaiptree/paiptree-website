2dc6ad53fb0ae4bb290f62083e59070ea4eea8b4
sundaynighttt <jazoo7777@gmail.com>
2026-02-05 12:23:25 +0900

    Add 3d_matrix page

---
+++ src/app/3d_matrix/page.tsx
@@ -0,0 +1,11 @@
+export default function Matrix3DPage() {
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <iframe
+        src="/3d_matrix/index.html"
+        title="3d_matrix"
+        style={{ width: '100%', height: '100%', border: 0 }}
+      />
+    </div>
+  );
+}
